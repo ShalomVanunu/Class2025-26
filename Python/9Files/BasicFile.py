e8bf9610db8c767cd29362ea44415269206ab127
ShalomVanunu <shalomvanunu@gmail.com>
2024-10-31 08:54:35 +0200

    Update

--- Python/9Files/BasicFile.py
+++ Python/9Files/BasicFile.py
@@ -1,6 +1,6 @@
 
 
-file  = open("names.txt", "a") # w - new file each time || a- append
+file  = open("names", "a") # w - new file each time || a- append
 file.write("shalom\n")
 file.write("oded\n")
 
