481ee0faf24ddd3f8c8d397fd19baf800c337020
ShalomVanunu <shalomvanunu@gmail.com>
2024-10-31 10:15:14 +0200

    Update

--- HTML/ReadHTML.py
+++ HTML/ReadHTML.py
@@ -5,7 +5,9 @@ URL = "https://hack-yourself-first.com/"
 
 url_content = requests.get(URL)
 
-url_content_post = requests.post(URL,{"Email":"shalom@gmail.com","Password":"qweasd"})
+url_content_post = requests.post(URL,data = {"Email":"shalom@gmail.com","Password":"qweasd"})
 
-if "Login" not in url_content_post.text: # check Home page
-    print("Succes ...graet")
+#print(url_content_post.text)
+
+if "Log in" not in url_content_post.text: # check Home page
+    print("Success ...great")
